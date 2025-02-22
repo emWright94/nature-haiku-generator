667c78c9a054aa6b3c69d9a319c7eeca55c3a32e
emWright94 <emwright94@gmail.com>
2025-02-22 16:23:23 -0500

    added js including plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Generating haiku...",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poemForm");
+poemFormElement.addEventListener("submit", generatePoem);
